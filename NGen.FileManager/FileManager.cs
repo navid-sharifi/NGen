ddaaa6f3162957063f289242b3fccb82c3fff811
Navid <navid0523@gmail.com>
2023-01-10 00:30:44 +0330

    no message

--- NGen.FileManager/FileManager.cs
+++ NGen.FileManager/FileManager.cs
@@ -2,12 +2,22 @@
 
 namespace NGen
 {
-    public class FileManager : AModule , DependentModule
+    public class FileManager : AModule , IndependentModule
     {
+        public (string name, string content) Controller()
+        {
+            throw new NotImplementedException();
+        }
+
         public override string GetReactPage(Type pageType, Type moduleType)
         {
             return  @$"";
 
+        }
+
+        public (string name, string content) ReactModule()
+        {
+            throw new NotImplementedException();
         }
     }
 }
